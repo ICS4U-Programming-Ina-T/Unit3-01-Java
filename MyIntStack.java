3b65c93c61ec62421c7cd02561d792414b261bcb
Ina Tolo <94471679+InaTolo@users.noreply.github.com>
2022-04-28 12:53:00 -0400

    Added code for push method and user input.

--- MyIntStack.java
+++ MyIntStack.java
@@ -1,8 +1,8 @@
 import java.util.Stack;
 
 /**
- * The MyIntStack program contains an empty constructor
- * the program.
+ * The MyIntStack program will perform commands
+ * used in Stack (push, pop, etc.)
  *
  * @author  Ina Tolo
  * @version 1.0
