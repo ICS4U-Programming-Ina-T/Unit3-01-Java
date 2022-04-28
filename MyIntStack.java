10eba1d832555f68c80b9ba6d6b9a5a250fad2f0
Ina Tolo <94471679+InaTolo@users.noreply.github.com>
2022-04-28 09:44:40 -0400

    Added code for push method and user input.

--- MyIntStack.java
+++ MyIntStack.java
@@ -16,13 +16,12 @@ public class MyIntStack {
     /**
      * Empty constructor for the class.
      */
-<<<<<<< HEAD
+
     public MyIntStack() { 
         _stack = new Stack<Integer>();
     }
-=======
+
     public MyIntStack() { }
->>>>>>> ac0cfdb17d8d1132186a2694d0a35065518606e2
 
     /**
      * Function that adds integer to top of stack.
@@ -31,6 +30,7 @@ public class MyIntStack {
      */
     void stackPush(int userNumInt) {
         _stack.push(userNumInt);
+
         // for testing purposes only
         System.out.println("*** " + userNumInt + " added to the stack.");
     }
